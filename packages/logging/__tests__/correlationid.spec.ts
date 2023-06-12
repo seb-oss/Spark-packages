cccc98d7858c38337f727da07469c4c0bee39222
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-06-12 14:23:21 +0200

    test: clean up test file

--- packages/logging/__tests__/correlationid.spec.ts
+++ packages/logging/__tests__/correlationid.spec.ts
@@ -1,29 +1,29 @@
 import { getCorrId, runWithCorrelationId } from '../src/lib/correlationid'
-describe('Test correlationId functionality', () => {
-  it('Test corrId created when not supplied', () => {
-    runWithCorrelationId(async () => {
-      expect(getCorrId()).toEqual(expect.any(String))
+
+test('corrId created when not supplied', () => {
+  runWithCorrelationId(async () => {
+    expect(getCorrId()).toEqual(expect.any(String))
+  })
+})
+
+test('existing corrId is used', () => {
+  runWithCorrelationId(async () => {
+    expect(getCorrId()).toEqual('imafish')
+  }, 'imafish')
+})
+
+test('corrId works in Promises', () => {
+  runWithCorrelationId(async () => {
+    const corrId = await new Promise((res) => {
+      res(getCorrId())
     })
-  })
 
-  it('Test existing corrId is used', () => {
-    runWithCorrelationId(async () => {
-      expect(getCorrId()).toEqual('imafish')
-    }, 'imafish')
-  })
+    expect(corrId).toEqual('imafish')
+  }, 'imafish')
+})
 
-  it('Test corrId works in Promises', () => {
-    runWithCorrelationId(async () => {
-      const corrId = await new Promise((res) => {
-        res(getCorrId())
-      })
-      expect(corrId).toEqual('imafish')
-    }, 'imafish')
-  })
-
-  it('Test undefined corrId is not saved', () => {
-    runWithCorrelationId(async () => {
-      expect(getCorrId()).toEqual(expect.any(String))
-    }, undefined)
-  })
+test('undefined corrId is not saved', () => {
+  runWithCorrelationId(async () => {
+    expect(getCorrId()).toEqual(expect.any(String))
+  }, undefined)
 })
