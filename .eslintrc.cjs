a80d50d14717219d3a07b2be490ba0968b52c72b
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-06-27 10:06:36 +0200

    chore: add test and lint commands

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,13 +8,4 @@ module.exports = {
   ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.spec.ts', '*.spec.tsx', '*.spec.js', '*.spec.jsx'],
-      env: {
-        jest: true,
-      },
-      rules: {},
-    },
-  ],
 }
--- jest.preset.js
+++
@@ -1,7 +0,0 @@
-const nxPreset = require('@nx/jest/preset').default
-
-module.exports = {
-  ...nxPreset,
-  collectCoverage: true,
-  coverageDirectory: `${process.env.NX_WORKSPACE_ROOT}/coverage/${process.env.NX_TASK_TARGET_PROJECT}`,
-}
