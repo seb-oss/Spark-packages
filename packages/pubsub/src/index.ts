13249a70eed559c98e7510048ae9048b83bc6ee7
Alexander Czigler <git@ilix.se>
2023-08-11 13:07:40 +0200

    fix: export config type

--- packages/pubsub/src/index.ts
+++ packages/pubsub/src/index.ts
@@ -1,5 +1,6 @@
 export { createPubsub } from './lib/pubsub'
 export { pushRouter } from './lib/router'
 
+export type { ClientConfig } from './lib/client'
 export type { PubSubTopic } from './lib/pubsub'
 export type { PubSubHeaders, Unsubscriber } from './lib/subscriber'
