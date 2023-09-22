a0054dadb5ef4f5f9bc264814500cebb933eafd7
Alexander Czigler <git@ilix.se>
2023-09-22 15:41:08 +0200

    test: pull and push cases

--- packages/pubsub/src/lib/pubsub.spec.ts
+++ packages/pubsub/src/lib/pubsub.spec.ts
@@ -80,7 +80,6 @@ const setup = async () => {
   subscription.get.mockImplementation(async () => [subscription])
   subscription.on.mockImplementation(
     (name: string, cb: (...args: unknown[]) => void) => {
-      if (name === 'push-message') subscriberFn = cb
       if (name === 'message') subscriberFn = cb
       return subscription
     },
@@ -252,7 +251,11 @@ describe('#topic', () => {
       expect(mockTopic.createSubscription).toHaveBeenCalledWith(
         topicName + '.test',
         {
-          pushConfig: expect.any(Object),
+          pushConfig: expect.objectContaining({
+            pushEndpoint: expect.stringMatching(
+              'http://localhost:8080/pubsub/push',
+            ),
+          }),
           retryPolicy: {
             minimumBackoff: {
               seconds: expect.any(Number),
@@ -293,10 +296,6 @@ describe('#topic', () => {
       })
 
       expect(subscription.on).toHaveBeenCalledWith(
-        'push-message',
-        expect.any(Function),
-      )
-      expect(subscription.on).toHaveBeenCalledWith(
         'error',
         expect.any(Function),
       )
@@ -311,16 +310,20 @@ describe('#topic', () => {
       unsubscribe()
 
       expect(subscription.off).toHaveBeenCalledWith(
-        'push-message',
-        expect.any(Function),
-      )
-      expect(subscription.off).toHaveBeenCalledWith(
         'error',
         expect.any(Function),
       )
     })
 
     describe('ack and nacking', () => {
+      afterAll(() => {
+        process.env.PUBSUB_DELIVERY_MODE = 'push'
+      })
+
+      beforeAll(() => {
+        process.env.PUBSUB_DELIVERY_MODE = 'pull'
+      })
+
       it('acks after completed', async () => {
         const { createdPubsub, topicName, ack, nack } = await setup()
 
