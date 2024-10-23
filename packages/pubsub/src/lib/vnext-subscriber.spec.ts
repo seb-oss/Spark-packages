57027907540159fdf5b4e94a325bd26a8a1e1ba1
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2024-10-23 15:58:20 +0200

    test(pubsub): subscriber

--- packages/pubsub/src/lib/vnext-subscriber.spec.ts
+++ packages/pubsub/src/lib/vnext-subscriber.spec.ts
@@ -1,85 +1,124 @@
-
-import { describe, it, vi, expect } from 'vitest'
-import type { PubSub } from '@google-cloud/pubsub'
-//import { createPublisher, createSubscriber } from './vnext'
+import { describe, it, vi, expect, type MockedObject, afterAll } from 'vitest'
+import { PubSub, type Subscription, type Topic } from '@google-cloud/pubsub'
 import { z } from 'zod'
-import { zodToAvro } from './zod-to-avro';
+import { createSubscriber } from './vnext-subscriber'
+import type { Schema } from 'avsc'
 
 const exampleSchema = z.object({
   messageType: z.string(),
   created: z.date(),
-  data: z.string().optional()
-});
+  data: z.string().optional(),
+})
 
-
-type ExampleMessage = z.infer<typeof exampleSchema>;
-const exampleAvroSchema = zodToAvro("Example", exampleSchema, { namespace: "com.acme.example" });
-
+type ExampleMessage = z.infer<typeof exampleSchema>
 type ExamplePubsubChannels = {
   example: ExampleMessage
 }
 
 vi.mock('@google-cloud/pubsub', () => {
+  const topics: { [key: string]: Topic } = {}
+  const mockTopic = vi.fn().mockImplementation((name: string) => {
+    if (topics[name]) {
+      return topics[name]
+    }
+
+    let subscriptionExists = false
+    const subscriptionMock = {
+      name: 'a-subscription',
+      exists: vi.fn().mockImplementation(() => [subscriptionExists]),
+      on: vi.fn(),
+    }
+
+    topics[name] = {
+      name,
+      get: vi.fn().mockImplementation(() => [topics[name]]),
+      createSubscription: vi.fn().mockImplementation(() => [subscriptionMock]),
+      subscription: vi.fn().mockImplementation((name) => {
+        if (name === 'existing-subscription') {
+          subscriptionExists = true
+        }
+
+        return subscriptionMock
+      }),
+    } as unknown as Topic
+    return topics[name]
+  })
+
+  const currentSchema = {
+    get: vi.fn().mockImplementation(() => ({ revisionId: 'a-revision-id' })),
+  }
+
+  const schema = vi.fn().mockImplementation(() => {
+    return currentSchema
+  })
+
   const pubsub: Partial<PubSub> = {
-    topic: vi.fn(),
+    topic: mockTopic,
+    createSchema: vi
+      .fn()
+      .mockImplementation(() => ({ id: 'first-revision-id' })),
+    schema: vi
+      .fn()
+      .mockImplementation((name) => schema(name) as unknown as Schema),
   }
 
   return {
     PubSub: vi.fn().mockReturnValue(pubsub),
+    SchemaTypes: {
+      ProtocolBuffer: 'PROTOCOL_BUFFER',
+      Avro: 'AVRO',
+    },
   }
 })
-  
 
-const description = "This is an example message";
+const description = 'This is an example message'
 const zodValue = z.object(
   {
     messageType: z.string(),
     message: z.number(),
   },
   { description }
-);
-const avroSchema = zodToAvro("ExampleMessage", zodValue);
+)
 
-// When client is set up with project id. PUBSUB is beeing called with the correct things.
-// When topic publish is run, we can se that correct data is beeing sent to the underlying things.
-// check that _topic.publishMessage({ json: message }) has been called.
+describe('subscriber', () => {
+  const topicName = 'example'
+  const subscriptionName = 'example-subscription'
 
-// When subsribe we can trigger the call back that is beeing registered.
+  it('creates a new subscription if it does not exist', async () => {
+    const topicMock = new PubSub().topic(topicName) as MockedObject<Topic>
 
-
-/*
-  things to test.
-  - Initiation is run with the correct parrams.
-  - Create subscrubtion
-  - Get subscription
-  - Process message?
-*/
-
-describe('when creating a new pubsub client the internal client', () => {
-  it('should be initiated with the configuration', async () => {
-
-      
-    /*const client = createPublisher<ExamplePubsubChannels>({
+    const subscriber = createSubscriber<ExamplePubsubChannels>({
       projectId: 'test',
     })
 
-    client.topic("example", exampleAvroSchema);
-    expect(PubSub).toBeCalledWith({
+    await subscriber.topic('example').subscribe(subscriptionName, () => {})
+
+    expect(topicMock.createSubscription).toHaveBeenCalled()
+    expect(topicMock.createSubscription).toHaveBeenCalledWith(
+      `example_${subscriptionName}`,
+      expect.any(Object)
+    )
+  })
+
+  it('uses an existing subscription if it exists', async () => {
+    const topicMock = new PubSub().topic(topicName) as MockedObject<Topic>
+    const subscriptionMock = topicMock.subscription(
+      subscriptionName
+    ) as MockedObject<Subscription>
+
+    subscriptionMock.exists.mockImplementation(() => [true])
+
+    const subscriber = createSubscriber<ExamplePubsubChannels>({
       projectId: 'test',
-    })*/
+    })
+
+    topicMock.createSubscription.mockClear()
+
+    await subscriber
+      .topic('example')
+      .subscribe('existing-subscription', () => {})
+
+    expect(topicMock.subscription).toHaveBeenCalled()
+    expect(topicMock.createSubscription.mock.calls.length).toBe(0)
   })
 })
-/*
-describe('when publishing on a topic the internal client is called with the correct values', () => {
-    it('should be initiated with the configuration', async () => {
-      const client = createSubscriber<ExamplePubsubChannels>({
-        projectId: 'test',
-      })
-      client.topic("example").publish({messageType: "the type", data: "Data goes here."});
-      
-      expect(PubSub).toBeCalledWith({
-        projectId: 'test',
-      })
-    })
-  })
-*/
