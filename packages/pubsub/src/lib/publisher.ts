eb46ac0521d5dd44b93d06fb63be8026ce0a5fee
Emil Cardell <emil.cardell@gmail.com>
2024-10-24 08:45:03 +0200

    feat: 🎸 Removes old client implementation and adds a new
    Removes the old implementation and adds a new to part client with a
    subscriber and a publisher.
    
    BREAKING CHANGE: 🧨 New interface not compatible with the old one.

--- packages/pubsub/src/lib/publisher.ts
+++ packages/pubsub/src/lib/publisher.ts
@@ -1,39 +1,131 @@
-import { type ClientConfig, getOrCreateTopic } from './client'
+import {
+  type ClientConfig,
+  type ISchema,
+  PubSub,
+  SchemaTypes,
+  type Topic,
+} from '@google-cloud/pubsub'
+import { Type } from 'avsc'
 
-type Publisher<T, Headers, raw> = (
-  message: T,
-  headers?: Headers,
-  rawMessage?: raw
-) => Promise<string>
+const schemaIdPattern = /^(?!goog)[a-zA-Z][a-zA-Z0-9-._~%+]{2,254}$/
 
-interface PubsubMessage<Message, Headers extends Record<string, unknown>> {
-  message: Message
-  headers?: Headers
+type CloudSchema = {
+  schemaId: string
+  avroDefinition: string
 }
 
-export const publisher =
-  <
-    Msg,
-    TopicName extends string | number | symbol,
-    Headers extends Record<string, unknown>,
-  >(
-    topicName: TopicName,
-    config?: ClientConfig
-  ): Publisher<Msg, Headers, boolean> =>
-  async (message, headers?, raw?) => {
-    const topic = await getOrCreateTopic(topicName.toString(), config)
-    const msg: PubsubMessage<Msg, Headers> = {
-      message,
-      headers,
-    }
-
-    let data: Buffer
-    if (raw) {
-      // Only send the message as the message.
-      data = Buffer.from(JSON.stringify(message))
-    } else {
-      data = Buffer.from(JSON.stringify(msg))
-    }
-
-    return topic.publishMessage({ data })
+const syncTopicSchema = async (client: PubSub, cloudSchema: CloudSchema) => {
+  if (!schemaIdPattern.test(cloudSchema.schemaId)) {
+    throw Error(
+      'schemaId is no in a valid format. Check google cloud platform for more information'
+    )
   }
+
+  const schema = await client.schema(cloudSchema.schemaId)
+
+  const exits = await schemaExists(client, cloudSchema.schemaId)
+  if (exits) {
+    const data = await schema.get()
+    return data
+  }
+
+  await client.createSchema(
+    cloudSchema.schemaId,
+    SchemaTypes.Avro,
+    cloudSchema.avroDefinition
+  )
+  const data = await schema.get()
+  return data
+}
+
+const createOrGetTopic = async (
+  client: PubSub,
+  name: string,
+  schemaData?: ISchema
+) => {
+  const [topic] = await client.topic(name).get({ autoCreate: true })
+
+  if (!schemaData) {
+    return topic
+  }
+
+  const [topicMetadata] = await topic.getMetadata()
+  const topicSchemaMetadata = topicMetadata.schemaSettings
+
+  await topic.setMetadata({
+    ...topicMetadata,
+    schemaSettings: {
+      encoding: 'JSON',
+      firstRevisionId:
+        topicSchemaMetadata?.firstRevisionId ?? schemaData.revisionId,
+      lastRevisionId: schemaData.revisionId,
+      schema: schemaData.name,
+    },
+  })
+
+  return topic
+}
+
+export type PublisherClient<T extends Record<string, unknown>> = {
+  topic<K extends keyof T>(
+    name: K,
+    cloudSchema?: CloudSchema
+  ): {
+    publish<M extends T[K]>(message: M): Promise<void>
+    initiate(): Promise<void>
+  }
+}
+
+export const createPublisher = <T extends Record<string, unknown>>(
+  clientOptions?: ClientConfig | undefined
+): PublisherClient<T> => {
+  const client = clientOptions ? new PubSub(clientOptions) : new PubSub()
+  let _topic: Topic
+  let _type: Type
+  const ensureInitiated = async (
+    name: string | number | symbol,
+    schema: CloudSchema | undefined
+  ) => {
+    if (!_topic) {
+      if (schema) {
+        const schemaData = await syncTopicSchema(client, schema)
+        _topic = await createOrGetTopic(client, name as string, schemaData)
+      }
+
+      _topic = await createOrGetTopic(client, name as string)
+    }
+    if (schema && !_type) {
+      const schemaType = Type.forSchema(JSON.parse(schema.avroDefinition))
+      _type = schemaType
+    }
+  }
+  const typedClient: PublisherClient<T> = {
+    topic: (name, schema) => {
+      return {
+        initiate: async () => {
+          return ensureInitiated(name, schema)
+        },
+        publish: async (json) => {
+          await ensureInitiated(name, schema)
+
+          if (_type) {
+            const data = _type.toBuffer(json)
+            await _topic.publishMessage({ data })
+          } else {
+            await _topic.publishMessage({ json })
+          }
+        },
+      }
+    },
+  }
+  return typedClient
+}
+
+const schemaExists = async (client: PubSub, schemaId: string) => {
+  for await (const s of client.listSchemas()) {
+    if (s.name === schemaId) {
+      return true
+    }
+  }
+  return false
+}
