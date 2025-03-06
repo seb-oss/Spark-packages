701f083d26bed020f5c92194b4bdce902b51b9de
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2025-03-06 16:29:39 +0100

    test: verify things work without specifying auth token functions

--- packages/openapi-client/src/test/authRetry.spec.ts
+++ packages/openapi-client/src/test/authRetry.spec.ts
@@ -1,75 +1,91 @@
-import nock from 'nock';
-import { describe, it, beforeEach, afterEach, vi, expect } from 'vitest';
-import { TypedClient } from '../client';
-import { ApiClient } from './api';
-import { AxiosError } from 'axios';
-import { HttpError } from '@sebspark/openapi-core';
+import nock from 'nock'
+import { describe, it, beforeEach, afterEach, vi, expect } from 'vitest'
+import { TypedClient } from '../client'
+import { ApiClient } from './api'
+import { AxiosError } from 'axios'
+import { HttpError } from '@sebspark/openapi-core'
 
 describe('Auth Retry Tests', () => {
-  const url = 'https://api.example.com';
+  const url = 'https://api.example.com'
 
   let server = nock(url)
 
   beforeEach(() => {
-    nock.cleanAll();
-  });
+    nock.cleanAll()
+  })
 
   afterEach(() => {
-    nock.cleanAll();
-    vi.clearAllMocks();
-  });
+    nock.cleanAll()
+    vi.clearAllMocks()
+  })
 
-  const authorizationTokenGeneratorMock = vi.fn();
-
-  const authorizationTokenRefreshMock = vi.fn();
+  const authorizationTokenGeneratorMock = vi.fn()
+  const authorizationTokenRefreshMock = vi.fn()
 
   
-  const apiClient = TypedClient<ApiClient>(url, {
-    authorizationTokenGenerator: authorizationTokenGeneratorMock,
-    authorizationTokenRefresh: authorizationTokenRefreshMock
-  });
-
-  it('should refresh token and retry on auth failure', async () => {
-    authorizationTokenGeneratorMock.mockResolvedValue({
-      'Proxy-auth': 'Bearer 123'
-     })
-
-    server
-      .get('/health')
-      .reply(401, { message: 'Unauthorized' })
-      .get('/health')
-      .reply(200, { });
-
-    // Your test logic here
-    await apiClient.get('/health');
-    expect(authorizationTokenRefreshMock).toHaveBeenCalledOnce();
-    expect(authorizationTokenGeneratorMock).toBeCalledTimes(2);
-
-    expect(server.isDone()).to.be.true;
-  });
-
-  it('should only retry once', async () => {
-    authorizationTokenGeneratorMock.mockResolvedValue({
-      'Proxy-auth': 'Bearer 123'
-     })
-
-    server
-      .get('/health')
-      .reply(401, { message: 'Unauthorized' })
-      .get('/health')
-      .reply(401, { message: 'Unauthorized' })
-
-    // Your test logic here
-    try {
-      await apiClient.get('/health');
-      expect(false).toBe(true);
-    } catch (error) {
-      expect((((error as HttpError).cause) as AxiosError).cause?.message).toBe('Unauthorized');
-
-      expect(authorizationTokenRefreshMock).toHaveBeenCalledOnce();
-      expect(authorizationTokenGeneratorMock).toBeCalledTimes(2);
-    }
+  describe('with authorization functions', () => {
+    const apiClient = TypedClient<ApiClient>(url, {
+      authorizationTokenGenerator: authorizationTokenGeneratorMock,
+      authorizationTokenRefresh: authorizationTokenRefreshMock
+    })
     
-    expect(server.isDone()).to.be.true;
-  });
-});
+    it('should refresh token and retry on auth failure', async () => {
+      authorizationTokenGeneratorMock.mockResolvedValue({
+        'Proxy-auth': 'Bearer 123'
+      })
+      
+      server
+      .get('/health')
+      .reply(401, { message: 'Unauthorized' })
+      .get('/health')
+      .reply(200, { })
+      
+      // Your test logic here
+      await apiClient.get('/health')
+      expect(authorizationTokenRefreshMock).toHaveBeenCalledOnce()
+      expect(authorizationTokenGeneratorMock).toBeCalledTimes(2)
+      
+      expect(server.isDone()).to.be.true
+    })
+    
+    it('should only retry once', async () => {
+      authorizationTokenGeneratorMock.mockResolvedValue({
+        'Proxy-auth': 'Bearer 123'
+      })
+      
+      server
+      .get('/health')
+      .reply(401, { message: 'Unauthorized' })
+      .get('/health')
+      .reply(401, { message: 'Unauthorized' })
+      
+      // Your test logic here
+      try {
+        await apiClient.get('/health')
+        expect(false).toBe(true)
+      } catch (error) {
+        expect((((error as HttpError).cause) as AxiosError).cause?.message).toBe('Unauthorized')
+        
+        expect(authorizationTokenRefreshMock).toHaveBeenCalledOnce()
+        expect(authorizationTokenGeneratorMock).toBeCalledTimes(2)
+      }
+      
+      expect(server.isDone()).to.be.true
+    })
+  })
+
+  describe('without authorization functions', () => {
+    const apiClient = TypedClient<ApiClient>(url)
+    
+    it('should work', async () => {
+      server
+      .get('/health')
+      .reply(200, { })
+      
+      // Your test logic here
+      await apiClient.get('/health')
+      
+      expect(server.isDone()).to.be.true
+    })
+  })
+})
