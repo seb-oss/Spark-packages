3b914ef96c5cad731cdb856da1683742177b6b25
Oskar Bernberg <oskar.bernberg.91@gmail.com>
2025-03-19 10:11:32 +0100

    feat: 🎸 Add trade insight package

--- packages/tradeinsight/src/createStockId.ts
+++ packages/tradeinsight/src/createStockId.ts
@@ -1,3 +1,12 @@
-export function createStockId(stock: string): string {
-  return stock
-}
+export const createStockId = ({
+  isin,
+  mic,
+  currency,
+}: { isin: string; currency: string; mic: string }) =>
+  `STOCK-${isin};${mic};${currency}`.toUpperCase()
+
+export const createForexId = ({
+  fromCurrency,
+  toCurrency,
+}: { toCurrency: string; fromCurrency: string }) =>
+  `FOREX-${toCurrency};${fromCurrency}`.toUpperCase()
