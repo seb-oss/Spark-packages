e38812bbf00b40c48ede9dee4612a828e4c7b371
Johan Öbrink <johan.obrink@gmail.com>
2025-10-24 18:45:21 +0200

    fix: removed jest config

--- packages/spanner-migrate/jest.e2e.config.ts
+++
@@ -1,9 +0,0 @@
-import baseConfig from './jest.config'
-
-export default {
-  ...baseConfig,
-  testMatch: ['**/*.e2e.ts'], // Only include E2E test files
-  testTimeout: 120000, // Set higher timeout (30 seconds in this case),
-  maxWorkers: 1, // Run tests sequentially (only 1 worker)
-  displayName: '@sebspark/spanner-migrate:e2e',
-}
