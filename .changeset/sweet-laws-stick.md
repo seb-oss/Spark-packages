03ec7dd88523a5f312bc44cbbb69bc661333ae50
Johan Öbrink <johan.obrink@gmail.com>
2025-12-05 16:39:52 +0100

    feat: esm only

---
+++ .changeset/sweet-laws-stick.md
@@ -0,0 +1,5 @@
+---
+"@sebspark/tradeinsight": major
+---
+
+ESM only
--- packages/tradeinsight/tsdown.config.ts
+++ packages/tradeinsight/tsdown.config.ts
@@ -1,5 +1,2 @@
 import config from '@sebspark/tsconfig/tsdown'
-export default {
-  ...config,
-  failOnWarn: false,
-} as typeof config
+export default {config}
