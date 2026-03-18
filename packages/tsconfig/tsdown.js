736a0b3895c3a770f7ee2f303b768f161ac54ba5
Johan Öbrink <johan.obrink@gmail.com>
2026-03-18 10:58:50 +0100

    fix: no bundling of packages in packages

--- packages/tsconfig/tsdown.js
+++ packages/tsconfig/tsdown.js
@@ -8,26 +8,11 @@ const userConfig = {
   dts: true,
   sourcemap: true,
   clean: true,
-  inlineOnly: false,
   inputOptions: {
     checks: {
       eval: false,
     },
   },
-  external: [
-    // full centralized list of CJS deps
-    '@opentelemetry/instrumentation',
-    '@opentelemetry/instrumentation-http',
-    '@opentelemetry/instrumentation-express',
-    '@opentelemetry/instrumentation-grpc',
-    '@opentelemetry/instrumentation-redis',
-    '@opentelemetry/instrumentation-dns',
-    '@opentelemetry/instrumentation-net',
-    '@opentelemetry/instrumentation-fs',
-    '@opentelemetry/instrumentation-undici',
-    '@opentelemetry/instrumentation-socket.io',
-    'require-in-the-middle',
-  ],
 }
 
 export default defineConfig(userConfig)
